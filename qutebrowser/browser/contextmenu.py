309932f711a7d8829e8c212141d56d53bbf3428c
midoriiro <alexis@bekhdadi.com>
2016-08-18 16:09:30 +0200

    Add Trigger enum

--- qutebrowser/browser/contextmenu.py
+++ qutebrowser/browser/contextmenu.py
@@ -6,6 +6,20 @@ from PyQt5.QtWidgets import QMenu, QAction, QActionGroup
 
 from qutebrowser.utils import usertypes, log
 
+Trigger = usertypes.enum('Trigger', [
+    'load_status',
+    'has_selection',
+    'has_link',
+    'has_tabs',
+    'can_insert',
+    'can_go_back',
+    'can_go_forward',
+    'can_go_to',
+    'can_undo',
+    'can_redo',
+    'has_extra_enabled'
+])
+
 
 class ContextMenu:
 
@@ -43,41 +57,41 @@ class ContextMenu:
         self._action_dict['common']['screenshot'] = {
             'text': 'Screenshot',
             'icon': None,
-            'trigger': 'loadStatus'
+            'trigger': Trigger.load_status
         }
 
         self._action_dict['search'] = collections.OrderedDict()
         self._action_dict['search']['for'] = {
             'text': 'Search for {}',
             'icon': None,
-            'trigger': 'hasSelection',
+            'trigger': Trigger.has_selection
         }
 
         self._action_dict['open'] = collections.OrderedDict()
         self._action_dict['open']['current'] = {
             'text': 'Open Link',
             'icon': None,
-            'trigger': 'hasLink',
+            'trigger': Trigger.has_link
         }
         self._action_dict['open']['tab'] = {
             'text': 'Open Link in New Tab',
             'icon': None,
-            'trigger': 'hasLink',
+            'trigger': Trigger.has_link
         }
         self._action_dict['open']['tab_background'] = {
             'text': 'Open Link in New Background Tab',
             'icon': None,
-            'trigger': 'hasLink',
+            'trigger': Trigger.has_link
         }
         self._action_dict['open']['window'] = {
             'text': 'Open Link in New Window',
             'icon': None,
-            'trigger': 'hasLink',
+            'trigger': Trigger.has_link
         }
         self._action_dict['open']['window_background'] = {
             'text': 'Open Link in New Background Window',
             'icon': None,
-            'trigger': 'hasLink',
+            'trigger': Trigger.has_link
         }
 
         self._action_dict['duplicate'] = collections.OrderedDict()
@@ -102,22 +116,22 @@ class ContextMenu:
         self._action_dict['move']['to_first'] = {
             'text': 'Move to First',
             'icon': None,
-            'trigger': 'hasTabs'
+            'trigger': Trigger.has_tabs
         }
         self._action_dict['move']['to_last'] = {
             'text': 'Move to Last',
             'icon': None,
-            'trigger': 'hasTabs'
+            'trigger': Trigger.has_tabs
         }
         self._action_dict['move']['to_left'] = {
             'text': 'Move to Left',
             'icon': None,
-            'trigger': 'hasTabs'
+            'trigger': Trigger.has_tabs
         }
         self._action_dict['move']['to_right'] = {
             'text': 'Move to Right',
             'icon': None,
-            'trigger': 'hasTabs'
+            'trigger': Trigger.has_tabs
         }
         self._action_dict['move']['to'] = {
             'text': 'Move to',
@@ -132,147 +146,148 @@ class ContextMenu:
         self._action_dict['close']['similar'] = {
             'text': 'Close Similar Tabs',
             'icon': None,
-            'trigger': 'hasTabs'
+            'trigger': Trigger.has_tabs
         }
         self._action_dict['close']['other'] = {
             'text': 'Close Other Tabs',
             'icon': None,
-            'trigger': 'hasTabs'
+            'trigger': Trigger.has_tabs
         }
         self._action_dict['close']['left'] = {
             'text': 'Close Left Tabs',
             'icon': None,
-            'trigger': 'hasTabs'
+            'trigger': Trigger.has_tabs
         }
         self._action_dict['close']['right'] = {
             'text': 'Close Right Tabs',
             'icon': None,
-            'trigger': 'hasTabs'
+            'trigger': Trigger.has_tabs
         }
 
         self._action_dict['link'] = collections.OrderedDict()
         self._action_dict['link']['copy_link'] = {
             'text': 'Copy Link',
             'icon': None,
-            'trigger': 'hasLink'
+            'trigger': Trigger.has_link
         }
         self._action_dict['link']['copy_image'] = {
             'text': 'Copy Image',
             'icon': None,
-            'trigger': 'hasLink'
+            'trigger': Trigger.has_link
         }
         self._action_dict['link']['copy_image_link'] = {
             'text': 'Copy Image Link',
             'icon': None,
-            'trigger': 'hasLink'
+            'trigger': Trigger.has_link
         }
         self._action_dict['link']['save_link'] = {
             'text': 'Save Link...',
             'icon': None,
-            'trigger': 'hasLink'
+            'trigger': Trigger.has_link
         }
         self._action_dict['link']['save_image'] = {
             'text': 'Save Image...',
             'icon': None,
-            'trigger': 'hasLink'
+            'trigger': Trigger.has_link
         }
 
         self._action_dict['clipboard'] = collections.OrderedDict()
         self._action_dict['clipboard']['cut'] = {
             'text': 'Cut',
             'icon': None,
-            'trigger': 'hasSelection'
+            'trigger': Trigger.has_selection
         }
         self._action_dict['clipboard']['copy'] = {
             'text': 'Copy',
             'icon': None,
-            'trigger': 'hasSelection'
+            'trigger': Trigger.has_selection
         }
         self._action_dict['clipboard']['paste'] = {
             'text': 'Paste',
             'icon': None,
-            'trigger': 'hasSelection|canPaste'
+            'trigger': Trigger.can_insert
         }
 
         self._action_dict['history'] = collections.OrderedDict()
         self._action_dict['history']['back'] = {
             'text': 'Go Back',
             'icon': None,
-            'trigger': 'canGoBack'
+            'trigger': Trigger.can_go_back
         }
         self._action_dict['history']['forward'] = {
             'text': 'Go Forward',
             'icon': None,
-            'trigger': 'canGoForward'
+            'trigger': Trigger.can_go_forward
         }
         self._action_dict['history']['to'] = {
             'text': 'Go to',
             'icon': None,
-            'trigger': 'canGoTo'
+            'trigger': Trigger.can_go_to
         }
 
         self._action_dict['stack'] = collections.OrderedDict()
         self._action_dict['stack']['undo'] = {
             'text': 'Undo',
             'icon': None,
-            'trigger': 'canUndo'
+            'trigger': Trigger.can_undo
         }
         self._action_dict['stack']['redo'] = {
             'text': 'Redo',
             'icon': None,
-            'trigger': 'canRedo'
+            'trigger': Trigger.can_redo
         }
 
         self._action_dict['loading'] = collections.OrderedDict()
         self._action_dict['loading']['reload'] = {
             'text': 'Reload',
             'icon': None,
-            'trigger': 'loadStatus'
+            'trigger': Trigger.load_status
         }
         self._action_dict['loading']['force_reload'] = {
             'text': 'Force Reload',
             'icon': None,
-            'trigger': 'loadStatus'
+            'trigger': Trigger.load_status
         }
         self._action_dict['loading']['stop'] = {
             'text': 'Stop',
             'icon': None,
-            'trigger': 'loadStatus'
+            'trigger': Trigger.load_status
         }
         self._action_dict['loading']['stop_all'] = {
             'text': 'Stop All',
             'icon': None,
-            'trigger': 'loadStatus'
+            'trigger': Trigger.load_status
         }
 
         self._action_dict['page'] = collections.OrderedDict()
         self._action_dict['page']['save'] = {
             'text': 'Save Page',
             'icon': None,
-            'trigger': 'loadStatus'
+            'trigger': Trigger.load_status
         }
         self._action_dict['page']['save_as'] = {
             'text': 'Save Page as...',
             'icon': None,
-            'trigger': 'loadStatus'
+            'trigger': Trigger.load_status
         }
 
         self._action_dict['bookmark'] = collections.OrderedDict()
         self._action_dict['bookmark']['link'] = {
             'text': 'Bookmark Link',
             'icon': None,
-            'trigger': 'hasLink'
+            'trigger': Trigger.has_link
         }
         self._action_dict['bookmark']['page'] = {
             'text': 'Bookmark Page',
-            'icon': None
+            'icon': None,
+            'trigger': Trigger.load_status
         }
 
         self._action_dict['extra'] = collections.OrderedDict()
         self._action_dict['extra']['inspect'] = {
             'text': 'Inspect Element',
             'icon': None,
-            'trigger': 'hasExtraEnabled'
+            'trigger': Trigger.has_extra_enabled
         }
 
         self._order = [k for k in self._action_dict.keys()]
@@ -281,16 +296,20 @@ class ContextMenu:
         self._menu = QMenu(widget)
 
         for section in self._order:
-            for action in self._action_dict[section].keys():
-                a = QAction(self._menu)
+            for k in self._action_dict[section].keys():
+                action = QAction(self._menu)
 
                 try:
-                    a.setCheckable(action['checkable'])
-                    a.setChecked(action['checked'])
+                    action.setCheckable(k['checkable'])
+                    action.setChecked(k['checked'])
                 except:
                     pass
                 else:
-                    a.setText(action['text'])
-                    a.setIcon(action['icon'])
+                    action.setText(k['text'])
+                    action.setIcon(k['icon'])
 
-                self._menu.addAction(a)
+                    k['object'] = action
+
+                self._menu.addAction(action)
+
+            self._menu.addSeparator()
